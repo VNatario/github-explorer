ef28190dd6378bf040cad39167dd8341a57768a8
Vitor Natario <natariovitor@gmail.com>
2022-08-25 18:27:39 -0300

    feat: add sass

--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,9 @@ module.exports = {
         use: "babel-loader",
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_module/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
