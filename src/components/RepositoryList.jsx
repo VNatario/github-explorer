be3885616462e9e5ece7cf1796ff5c566045c0ae
Vitor Natario <natariovitor@gmail.com>
2022-08-26 18:56:15 -0300

    feat: use api.github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/VNatario/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositório</h1>
