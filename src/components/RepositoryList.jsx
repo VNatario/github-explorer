c03def03d2a86e5f81c200fd7670d750380dfb50
Vitor Natario <natariovitor@gmail.com>
2022-08-26 09:26:38 -0300

    feat: create component RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositório</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">Acessar repostitório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">Acessar repostitório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">Acessar repostitório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
