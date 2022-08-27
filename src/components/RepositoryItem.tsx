f56194b1bbbe4b6bb26125d1d9a38f571c8c25ef
Vitor Natario <natariovitor@gmail.com>
2022-08-27 17:52:04 -0300

    feat: import interface

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,9 +1,7 @@
+import { Repository } from "./RepositoryList";
+
 interface RepositoryItemProps {
-  repository: {
-    name: string;
-    description: string;
-    html_url: string;
-  };
+  repository: Repository;
 }
 
 export function RepositoryItem(props: RepositoryItemProps) {
@@ -11,7 +9,6 @@ export function RepositoryItem(props: RepositoryItemProps) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description ?? "Sem descrição"}</p>
-
       <a target="_blank" href={props.repository.html_url}>
         Acessar repostitório
       </a>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,7 +3,7 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
-interface Repository {
+export interface Repository {
   name: string;
   description: string;
   html_url: string;
